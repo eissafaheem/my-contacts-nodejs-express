85fcd835b2a0134445bab1931c24220e34877a3f
Eissa Faheem <eissa.faheem@ziroh.com>
2023-08-21 17:13:07 +0530

    Feat: created mongoogse model

---
+++ models/contactModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const contactSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [
+                true,
+                "Please enter a name"
+            ]
+        },
+        email: {
+            type: String,
+            required: [
+                true,
+                "Please enter a email"
+            ]
+        },
+        phone: {
+            type: String,
+            required: [
+                true,
+                "Please enter a phone"
+            ]
+        }
+    },
+    {
+        timeStamp: true
+    }
+)
+
+module.exports = mongoose.model('Contact', contactSchema);
