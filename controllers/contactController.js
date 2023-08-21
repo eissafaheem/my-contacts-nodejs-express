ba8a34d8ac0574acb5a10c70115f12797b3fc6ea
Eissa Faheem <eissa.faheem@ziroh.com>
2023-08-21 17:06:56 +0530

    Feat: added async handler and connected mongodb

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,57 +1,59 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler((req, res) => {
     res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 
 // @desc Get one contact
 // @route GET /api/contacts
 // @access public
-const getOneContact = (req, res) => {
+const getOneContact = asyncHandler((req, res) => {
     res.status(200).json({ message: "Get one contact " + req.params.id });
-};
+});
 
 // @desc Insert one contact
 // @route POST /api/contacts
 // @access public
-const insertContact = (req, res) => {
-    console.log("req.body",req.body);
-    
+const insertContact = asyncHandler((req, res) => {
+    console.log("req.body", req.body);
+
     const {
         name,
         email,
         phone
     } = req.body;
 
-    if(!name && !email && !phone){
+    if (!name && !email && !phone) {
         res.status(404);
         throw new Error("All fields are mandatory !")
     }
-    
+
     res.status(200).json({ message: "Insert contact" });
-};
+});
 
 // @desc Update one contact by id
 // @route PUT /api/contacts
 // @access public
-const updateContact = (req, res) => {
-    res.status(200).json({ message: "Update contact "+req.params.id });
-};
+const updateContact = asyncHandler((req, res) => {
+    res.status(200).json({ message: "Update contact " + req.params.id });
+});
 
 // @desc Delete one contact by id
 // @route DELETE /api/contacts
 // @access public
-const deleteContact = (req, res) => {
-    res.status(200).json({ message: "delete contact "+req.params.id });
-};
+const deleteContact = asyncHandler((req, res) => {
+    res.status(200).json({ message: "delete contact " + req.params.id });
+});
 
 
-module.exports = { 
+module.exports = {
     getAllContacts,
     getOneContact,
     insertContact,
     updateContact,
     deleteContact
- };
+};
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,53 @@
-const errorHandler = (err, req, res, next) =>{
-    const statusCode = res.statusCode? res.statusCode: 500;
-    res.json({message: err.message, stackTrace: err.stack})
-} 
+const STATUS_CODES = require('../utils/constants');
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+
+    switch (statusCode) {
+        case STATUS_CODES.FORBIDDEN:
+            res.json({
+                title: "Forbidden",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+            
+            case STATUS_CODES.NOT_FOUND:
+            res.json({
+                title: "Not Found",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+            
+            case STATUS_CODES.SERVER_ERROR:
+            res.json({
+                title: "Server Error",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+            
+            case STATUS_CODES.UNAUTHORIZED:
+            res.json({
+                title: "Unauthorized",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+            
+            case STATUS_CODES.VALIDATION_ERROR:
+            res.json({
+                title: "Validation Error ",
+                message: err.message,
+                stackTrace: err.stack
+            });
+            break;
+
+        default:
+            break;
+    }
+
+}
 
 module.exports = errorHandler
